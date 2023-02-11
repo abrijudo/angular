6b80d3ed306969f426cb10b941df56f34fa1f934
Abraham <abrahamsenshi@gmail.com>
2023-02-11 19:34:54 +0100

    sad

--- proyectoAngular/src/app/app.module.ts
+++ proyectoAngular/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuArribaComponent } from './menu-arriba/menu-arriba.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuArribaComponent
   ],
   imports: [
     BrowserModule,
---
+++ proyectoAngular/src/app/menu-arriba/menu-arriba.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-arriba',
+  templateUrl: './menu-arriba.component.html',
+  styleUrls: ['./menu-arriba.component.css']
+})
+export class MenuArribaComponent {
+
+}
