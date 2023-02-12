82ad1560acbea359fcdaa52219c3fed1fd37af5a
Abraham <abrahamsenshi@gmail.com>
2023-02-12 18:09:14 +0100

    actu

--- proyectoAngular/src/app/pagina-productos/pagina-productos.component.ts
+++ proyectoAngular/src/app/pagina-productos/pagina-productos.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef   } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'pagina-productos',
@@ -6,5 +6,5 @@ import { Component, ViewChild, ElementRef   } from '@angular/core';
   styleUrls: ['./pagina-productos.component.css']
 })
 export class PaginaProductosComponent {
- 
+
 }
